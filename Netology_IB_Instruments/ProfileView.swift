aad1d033cc915ced62dcdc6845c146f0d31d01fb
Maria Gorozhanova <zvezdoz88@gmail.com>
2022-12-03 23:21:07 +0300

    Выполнена задача 1.2

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Maria Gorozhanova on 03.12.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var photo: UIImageView!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityOfBirthLabel: UILabel!
+
+    @IBOutlet weak var shortDescription: UITextView!
+}
